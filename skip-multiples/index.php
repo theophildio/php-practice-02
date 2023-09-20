fbe6f91f83d1f2bea20b80c37c44367a736d03b9
Theophil Dio <dtheo.sangma@gmail.com>
2023-09-20 12:53:52 +0600

    Added to repo of Skip multiples of 5

---
+++ skip-multiples/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Skip multiples of 5
+
+function skipMultiplesOf5($num)
+{
+    for ($i = 1; $i <= $num; $i++) {
+        if ($i % 5 == 0) {
+            continue;
+        } else {
+            echo "$i \n";
+        }
+    }
+}
+
+skipMultiplesOf5(50);
